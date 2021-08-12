eae2ead4e423a63122f41a081c4f4fe28e3e19ae
varen93 <varendersingh130@gmail.com>
2021-08-12 15:28:02 +0530

    changed the test path

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,8 @@
 
 set -eo pipefail
 
-xcodebuild -workspace WeatherAppMain\ WeatherApp.xcworkspace \
-            -scheme WeatherApp\ iOS \
+xcodebuild -workspace WeatherAppMain/ WeatherApp.xcworkspace \
+            -scheme WeatherApp \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
+
