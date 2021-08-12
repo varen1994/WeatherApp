a4305f3debfbcd12ef8f1f8eff03fca1a17117bf
varen93 <varendersingh130@gmail.com>
2021-08-12 15:19:28 +0530

    script changed

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace WeatherApp\ WeatherApp.xcworkspace \
+xcodebuild -workspace WeatherAppMain\ WeatherApp.xcworkspace \
             -scheme WeatherApp\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
